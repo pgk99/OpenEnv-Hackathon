021fa18a45ffca012fbf509b3143c4f525c9fa12
Pranav Kavishwar <pranavgk99@gmail.com>
2026-04-08 16:11:49 +0000

    Updated app.py file to make main() method callable

--- Aviation_Agent/server/app.py
+++ Aviation_Agent/server/app.py
@@ -54,6 +54,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
-    main(port=args.port)
+    main(host=args.host, port=args.port)
